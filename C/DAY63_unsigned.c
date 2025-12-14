ae0838678b054b7d69244b757474c8d7fbcfe79d
Rai Jen <raijenjb@gmail.com>
2025-12-14 23:48:57 +0800

    unsigned data type

---
+++ C/DAY63_unsigned.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    //! unsigned = can store only positive numbers
+    //! if negative  
+    signed int a = -5;
+    unsigned int b = -5;
+    unsigned int c = 2;
+    printf("signed int a: %d\n", a); //? -5
+    printf("bit: %u\n", a); //! 4294967291
+
+    printf("\nunsiged int b: %d\n", b); //? -5
+    printf("bit: %u\n", b); //! 4294967291
+
+    printf("\nunsigned int c: %d\n", c); //* 2
+    printf("bit: %u\n", c); //* 2
+    return 0;
+}
