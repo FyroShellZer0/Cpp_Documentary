71cbbdf200983b6f5dfd457eef5e8119463ca258
Rai Jen <raijenjb@gmail.com>
2025-12-17 17:32:39 +0800

    strcspn SAFE STRING

--- C/DAY58_strcspn.c
+++ C/DAY58_strcspn.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <string.h>
 int main(void) {
     char name[50];
     printf("Enter your name: ");
     fgets(name, sizeof(name), stdin);
-    name[strscpn(name, "\n")] = "\0";
+    name[strcspn(name, "\n")] = '0';
     // safer than name[strlen(name) - 1] = "\0";
     
     return 0;
